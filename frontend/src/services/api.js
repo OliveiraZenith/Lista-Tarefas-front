254a0e16090082308d6b4ceb71be7a7b4cbfefa4
Ana Laura Lis <analauralis.zenith@gmail.com>
2026-06-02 22:55:33 +0000

    Feat: adicionar dockerfile

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_URL?.replace(/\/$/, '') || ''
+const baseURL =
+  import.meta.env.VITE_API_URL?.replace(/\/$/, '') ||
+  '/api'
 
 export const api = axios.create({
   baseURL,
